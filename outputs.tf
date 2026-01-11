991c2d1a546e6e95f991aaf5441ed0ebe02fffb7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:07:54 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "servicebus_namespace_customer_managed_keys" {
+  description = "All servicebus_namespace_customer_managed_key resources"
+  value       = azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys
+}
+
