b68063c9e0dde985cc6751713a5167fdc01531c7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "servicebus_namespace_customer_managed_keys" {
-  description = "All servicebus_namespace_customer_managed_key resources"
-  value       = azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys
-}
 output "servicebus_namespace_customer_managed_keys_infrastructure_encryption_enabled" {
-  description = "List of infrastructure_encryption_enabled values across all servicebus_namespace_customer_managed_keys"
-  value       = [for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : v.infrastructure_encryption_enabled]
+  description = "Map of infrastructure_encryption_enabled values across all servicebus_namespace_customer_managed_keys, keyed the same as var.servicebus_namespace_customer_managed_keys"
+  value       = { for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : k => v.infrastructure_encryption_enabled }
 }
 output "servicebus_namespace_customer_managed_keys_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all servicebus_namespace_customer_managed_keys"
-  value       = [for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all servicebus_namespace_customer_managed_keys, keyed the same as var.servicebus_namespace_customer_managed_keys"
+  value       = { for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : k => v.key_vault_key_id }
 }
 output "servicebus_namespace_customer_managed_keys_namespace_id" {
-  description = "List of namespace_id values across all servicebus_namespace_customer_managed_keys"
-  value       = [for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : v.namespace_id]
+  description = "Map of namespace_id values across all servicebus_namespace_customer_managed_keys, keyed the same as var.servicebus_namespace_customer_managed_keys"
+  value       = { for k, v in azurerm_servicebus_namespace_customer_managed_key.servicebus_namespace_customer_managed_keys : k => v.namespace_id }
 }
 
